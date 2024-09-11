04fb5efe7c9ebdc763ffbddb1ac916a89e83fb0d
Rodrigo Nuñez <ronum.00@gmail.com>
2024-09-11 12:48:25 -0600

    Second working version with bad UI

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-// TODO: Add bass sound that starts from the beginning of the sound, which is the one that will be played when the user presses the spacebar
-let bass, gameBass, vocals, other, drums, piano
+let bass, drums, basePerc, shaker, guitarMain, guitarIntro, guitarHarmony
 let cx, cy
+// Constante para que el loop tenga sentido. Por alguna razón se exportaron más largos de lo que son
+const LOOP_DURATION = 4.17391304347826
+
 function preload() {
    soundFormats("mp3", "ogg")
    bass = loadSound("assets/Bass")
-   gameBass = loadSound("assets/GameBass")
-   vocals = loadSound("assets/Vocal")
-   other = loadSound("assets/Other")
-   drums = loadSound("assets/Drum")
-   piano = loadSound("assets/Piano")
+   drums = loadSound("assets/Drums")
+   basePerc = loadSound("assets/BasePerc")
+   shaker = loadSound("assets/Shaker")
+   guitarMain = loadSound("assets/GuitarMain")
+   guitarIntro = loadSound("assets/GuitarIntro")
+   guitarHarmony = loadSound("assets/GuitarHarm")
 }
 
-const soundsArePlaying = () =>
-   vocals.isPlaying() || other.isPlaying() || drums.isPlaying() || piano.isPlaying() || bass.isPlaying()
+const soundsArePlaying = () => bass.isPlaying() || drums.isPlaying() || basePerc.isPlaying() || shaker.isPlaying()
 
 function setup() {
    const cnv = createCanvas(600, 600)
@@ -23,6 +25,9 @@ function setup() {
    textSize(20)
    text("Click on the canvas to start", cx - 150, cy)
    text("Press spacebar to play/stop bass", cx - 150, cy + 40)
+   text("Press P to play/pause all sounds", cx - 150, cy + 80)
+   text("Press G to play a random guitar part on next loop", cx - 150, cy + 120)
+   setPlayMode()
 }
 
 function keyPressed() {
@@ -30,14 +35,9 @@ function keyPressed() {
       if (bass.isPlaying()) {
          bass.stop()
       } else {
-         if (!gameBass.isPlaying()) {
-            gameBass.play()
-         } else {
-            gameBass.stop()
-         }
+         bass.loop(undefined, undefined, undefined, undefined, LOOP_DURATION)
       }
    }
-
    if (key === "p" || key === "P") {
       if (soundsArePlaying()) {
          pauseAll()
@@ -45,20 +45,37 @@ function keyPressed() {
          playAll()
       }
    }
+
+   if (key === "g" || key === "G") {
+      const randomGuitar = random([guitarMain, guitarIntro, guitarHarmony])
+      randomGuitar.play(LOOP_DURATION - drums.currentTime())
+   }
 }
 
 const playAll = () => {
-   vocals.loop()
-   other.loop()
-   drums.loop()
-   piano.loop()
-   bass.loop()
+   guitarMain.play(undefined, undefined, undefined, undefined, LOOP_DURATION)
+   guitarIntro.play(LOOP_DURATION, undefined, undefined, undefined, LOOP_DURATION)
+   guitarHarmony.play(LOOP_DURATION * 2, undefined, undefined, undefined, LOOP_DURATION)
+   guitarMain.loop(LOOP_DURATION * 3, undefined, undefined, undefined, LOOP_DURATION)
+   bass.loop(LOOP_DURATION, undefined, undefined, undefined, LOOP_DURATION)
+   basePerc.loop(LOOP_DURATION, undefined, undefined, undefined, LOOP_DURATION)
+   shaker.loop(LOOP_DURATION, undefined, undefined, undefined, LOOP_DURATION)
+   drums.loop(LOOP_DURATION, undefined, undefined, undefined, LOOP_DURATION)
 }
 
 const pauseAll = () => {
-   vocals.pause()
-   other.pause()
-   drums.pause()
-   piano.pause()
-   bass.pause()
+   basePerc.stop()
+   shaker.stop()
+   drums.stop()
+   bass.stop()
+}
+
+const setPlayMode = () => {
+   bass.playMode("restart")
+   basePerc.playMode("restart")
+   shaker.playMode("restart")
+   drums.playMode("restart")
+   guitarHarmony.playMode("restart")
+   guitarIntro.playMode("restart")
+   guitarMain.playMode("restart")
 }
